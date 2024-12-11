988bf46013883f6ec8f6b16603db6c0e90a0ff98
logx <iamvarun158@gmail.com>
2024-12-12 02:12:05 +0530

    added method to get vault path

--- utils/obsidian.py
+++ utils/obsidian.py
@@ -1,6 +1,31 @@
 import os
+from utils.cypher.key import get_api_key
 
-def create_obsidian_note(note_title: str, note_content:str, vault_path: str, uploaded_file):
+def get_vault_path():
+    """
+    Determines the vault path for storing Markdown notes and assets, prioritizing accessibility.
+    Defaults to the user's Documents folder across platforms unless overridden by an API key.
+    """
+    # Try to get the vault path from the API key
+    vault_path = get_api_key("OBSIDIAN_VAULT_PATH")
+    flag = False
+    
+    if not vault_path:
+        flag = True
+        documents_dir = os.path.expanduser("~\\Documents")
+        vault_path = os.path.join(documents_dir, "ANOTAR")
+        
+        if not os.path.exists(vault_path):
+            os.makedirs(vault_path)
+    else:
+        vault_path = vault_path.replace('/', '\\')
+        if vault_path[-1] == '\\':
+            vault_path = vault_path[:-1] 
+    
+    return vault_path, flag
+
+
+def create_obsidian_note(note_title: str, note_content: str, vault_path: str, uploaded_file):
     """
     Adds uploaded file and note content to an Obsidian note. Creates a new note if it doesn't exist.
     
@@ -8,6 +33,8 @@ def create_obsidian_note(note_title: str, note_content:str, vault_path: str, upl
     :param vault_path: Path to the Obsidian vault.
     :param uploaded_file: File-like object of the uploaded image.
     """
+
+    print(f"Creating Notes...")
 
     if not os.path.exists(vault_path):
         print(f"'{vault_path}' does not exist. Creating Folder...")
@@ -32,10 +59,10 @@ def create_obsidian_note(note_title: str, note_content:str, vault_path: str, upl
         out_file.write(uploaded_file.getbuffer())
     
     image_markdown = f"![{image_filename}]({image_filename})\n"
+    note_content = note_content.replace('\[\n', '$').replace('\n\]', '$').replace('\\[ ', '$').replace(' \\]', '$').replace('\\[', '$').replace('\\]', '$')
     
-    with open(note_path, 'a') as note_file:
-        note_file.write('\n' + image_markdown + '\n')
-        note_file.write(note_content)
-        note_file.write('\n---\n')
+    with open(note_path, 'a', encoding='utf-8') as note_file:
+        note_file.write('\n---\n' + '\n' + image_markdown + '\n' + note_content + '\n' + '\n---\n')
     
+    print(f"Notes created successfully!")
     return note_path
