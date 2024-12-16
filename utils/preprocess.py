409b07794d71ec2e7612181f70f214babe105dcd
logx <iamvarun158@gmail.com>
2024-12-16 18:36:19 +0530

    Small fixes

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,4 +1,4 @@
-import easyocr, re
+import easyocr, re, os
 import pypdfium2 as pdfium
 from PIL import Image
 
@@ -11,51 +11,50 @@ def ocr(image_path: str):
         print("Error during OCR:", e)
 
 def preprocess_pdf(pdf, ocr_enhance=False):
-    try:
-        text = ""
-        ocr_result = ""
-        pages = pdfium.PdfDocument(pdf)
-        for page in pages:
-            t = page.get_textpage().get_text_range()
-            if t:
-                text += t
+    text = ""
+    ocr_result = ""
+    pages = pdfium.PdfDocument(pdf)
+    for page in pages:
+        t = page.get_textpage().get_text_range()
+        if t:
+            text += t
 
-        alpha_chars = len(re.findall(r'[a-zA-Z]', text))
-        if alpha_chars / len(text) < 0.3:
-            ocr_enhance = True
+    alpha_chars = len(re.findall(r'[a-zA-Z]', text))
+    if len(text) == 0 or alpha_chars / len(text) < 0.3:
+        ocr_enhance = True
 
-        if ocr_enhance:
-            for i in range(len(pages)):
-                name = f"./ocr/page{i}.png"
-                page = pages[i]
-                image = page.render(scale=4).to_pil()
-                image.save(f'{name}')
-                ocr_result += ocr(f'{name}') + '\n'
-                os.remove(f'{name}')
+    if ocr_enhance:
+        for i in range(len(pages)):
+            name = f"./ocr/page{i}.png"
+            page = pages[i]
+            image = page.render(scale=4).to_pil()
+            image.save(f'{name}')
+            ocr_result += ocr(f'{name}') + '\n'
+            os.remove(f'{name}')
 
         return text, None, ocr_result
 
-    except Exception as e:
-        print("Error during PDF text extraction:", e)
-
 def preprocess_image(image):
     ocr_result = ""
-    try:
-        name = "./ocr/ocr.png"
-        image = Image.open(image)
-        image.save(f'{name}')
-        image_path = f"{name}"
-        ocr_result += ocr(image_path)
+    name = "./ocr/ocr.png"
+    image = Image.open(image)
+    image.save(f'{name}')
+    image_path = f"{name}"
+    ocr_result += ocr(image_path)
 
-        return None, image_path, ocr_result
-
-    except Exception as e:
-        print("Error during image OCR:", e)
+    return None, image_path, ocr_result
 
 def preprocess_file(file, ocr_enhance=False):
     print("Processing file...")
     if file.name.lower().endswith('.pdf'):
-        return preprocess_pdf(file, ocr_enhance)
+        results = preprocess_pdf(file, ocr_enhance)
     else:
-        return preprocess_image(file)
+        results = preprocess_image(file)
+
+    if results:
+        print("Preprocessing completed.")
+        return results
+    else:
+        print("Preprocessing failed.")
+        return None, None, None
         
